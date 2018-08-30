a8189ba6eb50cf1e03b9dfb704b19de766f34216
Capitan Muffin <miakyatto@gmail.com>
2018-08-30 14:29:46 -0300

    seleccion por habitat terminado

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // CONFIG
 const pokedex = document.getElementById('pokedex');
 const BASE_API = 'https://pokeapi.co/api/v2/';
+let stopper = false;
 
 // DOM del Modal
 const modalContent = document.getElementById('modal-content');
@@ -14,16 +15,23 @@ const loaderImg = document.getElementById('loader-img');
 
 // DOM de botones
 const allPokeBtn = document.getElementById('btn-all-pk');
-const btnTypeList = document.getElementById('btn-type-pk');
-const btnHabitatList = document.getElementById('btn-habitat-pk');
-btnTypeList.addEventListener('click', typeListBtns);
-allPokeBtn.addEventListener('click', getAllPokemons);
-btnHabitatList.addEventListener('click', habitatListBtns);
+const btnFilterList = document.querySelectorAll('.btn-filter');
+
+	// Escucha de eventos en botones
+	allPokeBtn.addEventListener('click', getAllPokemons);
+	
+	// Botones de Filtrado
+	for(let i = 0; i < btnFilterList.length; i++){
+		btnFilterList[i].addEventListener('click', filterListBtns);
+	}
+
+	// Ciclo para colorear boton activo
 
 	// Lista de Botones
 	btnMenuList = document.querySelectorAll('li');
+	// Evento
 	for(let i = 0; i < btnMenuList.length; i++){
-		btnMenuList[i].addEventListener('click', function(e){
+		btnMenuList[i].addEventListener('click', function(){
 			for(let i = 0; i < btnMenuList.length; i++){
 				btnMenuList[i].classList.remove('active');
 			}
@@ -34,31 +42,39 @@ btnHabitatList.addEventListener('click', habitatListBtns);
 
 // Boton para cerrar modal
 const btnClose = document.getElementById('btn-close');
-btnClose.addEventListener('click', function(e){
+btnClose.addEventListener('click', closeModal)
+
+// ====== FUNCIONES MODAL ======
+
+function closeModal(e){
+	// funcion para cerrar el modal al clicar boton
 	e.preventDefault();
 	modal.classList.remove('modalIn');
 	modal.classList.add('modalOut');
 	setTimeout(function(){
 		overlay.classList.toggle('overlayOn');
 	}, 1000);
-})
-
-
-
-function alerta(){
-	// Funcion de testeo de eventos
-	alert('funciona');
 }
+
+function openModal(){
+	// funcion para mostrar el modal
+	overlay.classList.toggle('overlayOn');
+	modal.classList.remove('modalOut');
+	modal.classList.add('modalIn');
+}
+
+// ====== FUNCIONES RENDERIZADO ======
 
 // Funcion para renderizar pokemons despues de hacer la consulta
 function renderPokemon (data){
 	let container = document.createElement("div");
 	container.setAttribute('data-id', data.id);
 	container.classList.add("pokemon");
-	let capitalName = data.name.charAt(0).toUpperCase() + data.name.slice(1); 
+	let sprite = data.sprites.front_default;
+	let name = capitalize(data.name); 
 	container.innerHTML = `
-		<img src="${data.sprites.front_default}" alt="pokemon">
-		<h3>${capitalName}</h3>
+		<img src="${sprite}" alt="pokemon">
+		<h3>${name}</h3>
 	`;
 
 	container.addEventListener('click', renderModal);
@@ -66,39 +82,55 @@ function renderPokemon (data){
 }
 
 // Funcion para renderizar botones
-function renderTypeButtons(data){
-	let container = document.createElement("div");
-	container.setAttribute('data-type', data.name);
-	container.classList.add("type-button");
-	let capitalName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-	container.innerHTML = `
-			<img src="img/${data.name}-type.png" alt="" />
-			<h3>${capitalName}</h3>
-		`;
-	container.addEventListener('click', getTypeofPokemon);
-	pokedex.appendChild(container);
+
+async function filterListBtns(){
+	// Obtiene lista de botones de filtrado y los manda a renderizar
+	category = this.dataset.category;
+	if(!stopper){
+		loaderCount.innerHTML = "";
+		loader.classList.remove('loader-on');
+	}
+	randomPika(pokedex);
+	stopper = true;
+	console.log(`Searching ${category} list`);
+	await fetch(`${BASE_API}${category}/`)
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(data){
+			stopper = false;
+			pokedex.innerHTML = "";
+			console.log(`Loading ${category} list`);
+			for(let i = 0; i < data.results.length; i++){
+				if (stopper) {return;}
+				renderFilterButtons(data.results[i], category);
+			}
+			console.log('Done');
+		})
+		.catch(function(e){
+			console.log('error: -->' + e);
+		})
 }
 
-// Funcion botones habitat
-function renderHabitatButtons(data){
+function renderFilterButtons(data, category){
+	// Muestra en pantalla los botones de filtrado
 	let container = document.createElement("div");
-	container.setAttribute('data-type', data.name);
-	container.classList.add("type-button");
-	let capitalName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+	container.setAttribute('data-category', `${category}`);
+	container.setAttribute('data-name', data.name);
+	container.classList.add("filter-button");
+	let name = capitalize(data.name);
 	container.innerHTML = `
-			<img src="img/${data.name}-type.png" alt="" />
-			<h3>${capitalName}</h3>
+			<img src="img/${data.name}-${category}.png" alt="" />
+			<h3>${name}</h3>
 		`;
-	container.addEventListener('click', getHabitatofPokemon);
+	container.addEventListener('click', getFilteredPokemons);
 	pokedex.appendChild(container);
 }
 
 // Funcion para renderizar pokemon dentro del modal
-async function renderModal(e){
+async function renderModal(){
 		let pokeID = this.dataset.id;
-		overlay.classList.toggle('overlayOn');
-		modal.classList.remove('modalOut');
-		modal.classList.add('modalIn');
+		openModal();
 		randomPika(modalContent);
 		await fetch(`${BASE_API}pokemon/${pokeID}`)
 			.then(function(response){
@@ -120,10 +152,10 @@ async function renderModal(e){
 						generacion = data2.generation.name;
 					})
 					.catch(function(e){
-						console.log('Hubo un error : -->' + e);
+						console.log('error : -->' + e);
 					})
 
-				let capitalName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+				let name = capitalize(data.name);
 				let weight = data.weight / 10;
 				let height = data.height / 10;
 				let types = data.types;
@@ -148,7 +180,7 @@ async function renderModal(e){
 					<div class="modal-name">
 						<div class="name">
 							<span>#${data.id}</span>
-							<h2>${capitalName}</h3>
+							<h2>${name}</h3>
 						</div>
 						<div class="type-img">
 							${typesSTRname}
@@ -189,14 +221,13 @@ async function renderModal(e){
 				`;
 			})
 			.catch(function(e){
-				console.log('Hubo un error: -->' + e);
+				console.log('error: -->' + e);
 			})
 
 	}
 
 
-// Funciones y variables para pedir pokemones
-let stopper = false;
+// ====== FUNCIONES CONSULTAS POKEMON ======
 
 // Todos los pokemones
 async function getAllPokemons(){
@@ -252,13 +283,15 @@ async function getAllPokemons(){
 			}, 4000);
 		})
 		.catch(function(e){
-			console.log('Hubo un error: -->' + e);
+			console.log('error: -->' + e);
 		})
 }
 
 // Pokemones por tipo
-async function getTypeofPokemon(e){
-	let typePK = this.dataset.type;
+async function getFilteredPokemons(){
+	let category = this.dataset.category;
+	let type = this.dataset.name;
+	
 	if(!stopper){
 		loaderCount.innerHTML = "";
 		loader.classList.remove('loader-on');
@@ -269,25 +302,35 @@ async function getTypeofPokemon(e){
 	loader.classList.add('loader-on');
 	loaderCount.innerHTML = 'Obteniendo pokemones';
 
-	console.log(`pidiendo lista de pokemones tipo ${typePK}`);
+	console.log(`pidiendo lista de pokemones ${category}: ${type}`);
 
-	await fetch(`${BASE_API}type/${typePK}`)
+	await fetch(`${BASE_API}${category}/${type}`)
 		.then(function(response){
 			console.log("lista obtenida");
 			return response.json();
 		})
 		.then(async function(data){
 			stopper = false;
-			let numPokes = data.pokemon.length;
-			let pokemones = data.pokemon;
+			let pokemonList;
+			switch(category){
+				case 'pokemon-habitat':
+					pokemonList = data.pokemon_species;
+					break;
+				
+				case 'type':
+					pokemonList = data.pokemon;
+					break;
+			}
+			let numPokes = pokemonList.length;
 
-			console.log("renderizando pokemones");
+			console.log("rendering pokemons");
 			loaderCount.innerHTML = 'Cargando';
 			pokedex.innerHTML = "";
-
 			for(let i = 0; i < numPokes; i++){
 				if (stopper) {return;}
-				await fetch(`${BASE_API}pokemon/${pokemones[i].pokemon.name}`)
+				if(category == 'type'){
+
+					await fetch(`${BASE_API}pokemon/${pokemonList[i].pokemon.name}`)
 					.then(function(response){
 						return response.json();
 					})
@@ -299,6 +342,22 @@ async function getTypeofPokemon(e){
 						console.log(e);
 					})
 
+				} else if( category == 'pokemon-habitat'){
+
+					await fetch(`${BASE_API}pokemon/${pokemonList[i].name}`)
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(data){
+						if (stopper) {return;}
+						renderPokemon(data);
+					})
+					.catch(function(e){
+						console.log(e);
+					})
+
+				}
+				
 				loaderCount.innerHTML = `${i +1} / ${numPokes}`;
 			}
 
@@ -310,74 +369,24 @@ async function getTypeofPokemon(e){
 			}, 4000);
 		})
 		.catch(function(e){
-			console.log('Hubo un error: -->' + e);
+			console.log('error: -->' + e);
 		})
 }
 
-// Pokemones por habitat
+// ====== FUNCIONES EXTRA ======
 
-async function getHabitatofPokemon(){
-
+// Funcion capitalizar nombres
+function capitalize(text){
+	capitalText = text.charAt(0).toUpperCase() + text.slice(1);
+	return capitalText;
 }
 
-// Renderizado de botones de HABITAT
-async function habitatListBtns(){
-	if(!stopper){
-		loaderCount.innerHTML = "";
-		loader.classList.remove('loader-on');
-	}
-	randomPika(pokedex);
-	stopper = true;
-	console.log('Buscando lista de Habitats');
-	await fetch(`${BASE_API}pokemon-habitat/`)
-		.then(function(response){
-			return response.json()
-		})
-		.then(function(data){
-			stopper = false;
-			pokedex.innerHTML = "";
-			console.log('Cargando lista de Habitats');
-			for(let i = 0; i < data.results.length; i++){
-				if (stopper) {return;}
-				renderTypeButtons(data.results[i]);
-			}
-			console.log('Lista de Habitats cargada');
-		})
-		.catch(function(e){
-			console.log('Hubo un error: -->' + e);
-		})
-}
-
-// Renderizado de botones de TIPO
-async function typeListBtns(){
-	if(!stopper){
-		loaderCount.innerHTML = "";
-		loader.classList.remove('loader-on');
-	}
-	randomPika(pokedex);
-	stopper = true;
-	console.log('Buscando lista de Tipos');
-	await fetch(`${BASE_API}type/`)
-		.then(function(response){
-			return response.json();
-		})
-		.then(function(data){
-			stopper = false;
-			pokedex.innerHTML = "";
-			console.log('Cargando lista de Tipos');
-			for(let i = 0; i < data.results.length; i++){
-				if (stopper) {return;}
-				renderTypeButtons(data.results[i]);
-			}
-			console.log('Lista de tipos cargada');
-		})
-		.catch(function(e){
-			console.log('Hubo un error: -->' + e);
-		})
+// Funcion de testeo de eventos
+function alerta(){
+	alert('funciona');
 }
 
 // funcion pikachu loader random
-
 function randomPika(cont){
 	displayCont = cont.dataset.loader;
 	let num = Math.random() * 10;
